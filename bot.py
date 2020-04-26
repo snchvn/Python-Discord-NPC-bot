a18e771c94d330745793b268cbd70e51a46077ba
San <san@san.is>
2020-04-26 06:33:06 +0100

    version 1.0

--- bot.py
+++ bot.py
@@ -1,9 +1,5 @@
 import discord
-import asyncio
 import random
-from discord.ext import commands
-from discord.ext.commands import bot
-from discord.utils import get
 
 # Mission giver Discord bot. Randomly allocates pre-written mission scripts to members of voice channel.
 
@@ -13,7 +9,6 @@ def read_token():
         lines = f.readlines()
         return lines[0].strip()
 token = read_token()
-print('Token accepted')
 
 # Read the missions.txt file with all the different missions [INSERT MISSION PARAMETERS IN THIS FILE]
 def read_missions():
@@ -21,20 +16,21 @@ def read_missions():
         lines = [i.strip() for i in f]
         return lines
 missions = read_missions()
-print('Mission accepted')
 
 # Discord API calls begin here
 client = discord.Client()
-
 @client.event
 async def on_message(message):
     
-    channels = ["bot-test"]
+   # Use below code to define mission invocation by user role
    # valid_users = ["$ROLES"]
    # if str(message.channel) in channels and str(message.author) in valid_users:
-    
+    channels = ["bot-test"]
     if str(message.channel) in channels:   
+        
+        # Setting the !risk command
         if message.content == "!risk":
+            
             # Shuffle missions every time !risk is called
             random.shuffle(missions)
             
@@ -44,18 +40,16 @@ async def on_message(message):
             players = []
             for member in members:
                 players.append(str(member.id))
-        
             # Allocate missions to players
             for i in range(0,len(players)):
-                user = discord.utils.get(client.get_all_members(), id=players[i])
-                if user is not None:
-                    messageContent = ("<@" + str(players[i+1]) + "> your mission is to " + missions[i])
-                    await user.send(messageContent)
-                    print(messageContent)
-                user = discord.utils.get(client.get_all_members(), id=305136156087943179) # DIRECT USER ID IS WORKING BUT NOT players[i] VARIABLE.
-                messageContent = ("<@" + str(players[0]) + "> your mission is to " + missions[0])
-                print(messageContent)
+                
+                # Get the user element from the iterable player ID (STUCK HERE FOR 2 HOURS PASSING INTEGER AS STRING)
+                user = discord.utils.get(client.get_all_members(), id=int(players[i]))
+                # Compose message for each user allocating their respective mission
+                messageContent = ("<@" + str(players[i]) + "> your mission is to " + missions[i])
+                #Send a DM to the player
                 await user.send(messageContent)
-                await message.channel.send("All mission orders have been issued.")
+
+            await message.channel.send("Mission orders have been issued.")
 
 client.run(token)
